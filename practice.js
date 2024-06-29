6e23ae6544c26f9c3ac13eba1ea551860418148c
anikatasnin <jarintasnin09@gmail.com>
2024-06-29 12:50:40 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,36 @@
+// first task
+var momGave = 1000;
+console.log(momGave);
+
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+
+var returnMoney = momGave - totalPrice;
+console.log(returnMoney);
+
+// second task
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bangla = 99.50;
+
+var avgMarks = (math + bio + chem + phy + bangla) / 5;
+avgMarks = avgMarks.toFixed(2);
+console.log(avgMarks);
+ 
+// third task
+var firstString = 'I am going to be';
+var secondString = 'an awesome web developer';
+
+var combine = firstString +' '+ secondString;
+console.log(combine);
+
+// fourth task
+var firstNum = 119;
+var secondNum = 5;
+
+var remainder = firstNum % secondNum;
+console.log(remainder);
